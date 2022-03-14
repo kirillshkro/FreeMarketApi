0d0439a85d881c6a029e10d53934229934dc06e5
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2022-03-14 16:50:25 +0300

    Changed settings: added app.

--- FreeMarketApi/settings.py
+++ FreeMarketApi/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'rest_framework',
+    'freemarketapi.apps.FreemarketapiConfig'
 ]
 
 MIDDLEWARE = [
