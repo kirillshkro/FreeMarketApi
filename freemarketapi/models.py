8f16a110bd6cb048ad50a6c79da077042ac8f854
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2022-03-14 17:26:22 +0300

    Added model Product

--- freemarketapi/models.py
+++ freemarketapi/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255, null=False, blank=False, db_index=True,
+                             verbose_name='Наименование')
+    price = models.DecimalField(max_digits=8, decimal_places=2, null=False,
+                                verbose_name='Цена')
+    quantity = models.PositiveIntegerField(null=False, verbose_name='Количество')
+    description = models.TextField(null=False, blank=True)
+    image = models.ImageField(blank=True, null=False, upload_to='images/')
+    idx_title = models.Index(fields=('title',))
+
+    def __str__(self):
+        return self.title
