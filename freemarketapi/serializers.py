41f562c7cc93c9dcc53b35fba9ec02fc7496d9b0
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2022-06-19 15:48:37 +0300

    Added new complex field

--- freemarketapi/serializers.py
+++ freemarketapi/serializers.py
@@ -1,5 +1,6 @@
-from freemarketapi.models import *
 from rest_framework import serializers
+
+from freemarketapi.models import *
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -9,6 +10,8 @@ class ProductSerializer(serializers.ModelSerializer):
 
 
 class ReviewSerializer(serializers.ModelSerializer):
+    product = serializers.StringRelatedField(many=True)
+
     class Meta:
         model = Review
         fields = ('id', 'title', 'content', 'rating', 'product')
@@ -21,12 +24,23 @@ class ProductPhotoSerializer(serializers.ModelSerializer):
 
 
 class OrderSerializer(serializers.ModelSerializer):
+    total_sum = serializers.SerializerMethodField(method_name='get_total_sum')
+
+    def get_total_sum(self, order: Order) -> str:
+        from decimal import Decimal
+        summa: Decimal = Decimal(0.)
+        for product in order.product_set:
+            summa += Decimal(product.price)
+        return str(summa)
+
     class Meta:
         model = Order
-        fields = ('id', 'user', 'comment')
+        fields = ('id', 'user', 'comment', 'total_sum')
 
 
 class MarketUserSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(many=False)
+
     class Meta:
         model = MarketUser
         fields = ('id', 'user', 'phone')
