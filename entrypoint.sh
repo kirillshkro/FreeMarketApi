42e8194b8d124f001de5ce2d69563dc483d9e9c8
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2022-06-19 15:26:11 +0300

    Added configuration files for uwsgi and nginx

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ "$DATABASE" = "postgres" ]; then
+  echo "Waiting to start"
+
+  while ! nc -z "$SQL_HOST" "$SQL_PORT"; do
+      sleep 0.1
+  done
+
+  echo "PostgreSQL started"
+fi
+
+exec "$@"
